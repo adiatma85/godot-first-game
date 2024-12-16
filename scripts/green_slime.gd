bbe2ca1d1e0f45fe37bfd8adebf666e4b1ac80e2
ramdani koernia <adiatma85@gmail.com>
2024-12-16 22:04:33 +0700

    add enemy moving animation scripts

--- scripts/green_slime.gd
+++ scripts/green_slime.gd
@@ -1,21 +1,22 @@
-extends Area2D
+extends Node2D
 
+const SPEED: int = 60
 
-@onready var timer: Timer = $"../Timer"
+var direction: int = 1
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("Green Slime Summoned")
-	pass # Replace with function body.
+@onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var ray_cast_left: RayCast2D = $RayCastLeft
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-
-func _on_body_entered(_body: Node2D) -> void:
-	print("Kenak Slime")
-	timer.start()
-	pass # Replace with function body.
-
-
-func _on_timer_timeout() -> void:
-	print("JANCOK")
-	#get_tree().reload_current_scene()
-	pass # Replace with function body.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	
+	
+	position.x += direction * SPEED * delta
