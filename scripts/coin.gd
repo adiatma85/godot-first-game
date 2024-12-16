74eae2f3004d055da89c983220b594d3c83c1b74
ramdani koernia <adiatma85@gmail.com>
2024-12-16 23:20:17 +0700

    add scoring

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,15 +1,7 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var iniString: String = "I am a Coin Jasik"
-	print(iniString)
-	pass # Replace with function body.
-
-
-func _on_body_entered(_body: Node2D) -> void:
-	var iniStringCoin: String = "+1 Coin"
-	print(iniStringCoin)
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
 	queue_free()
-	pass # Replace with function body.
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const SPEED: int = 200.0
+const JUMP_VELOCITY: int = -300.0
 
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,12 +13,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Result input direction in -1, 0, 1
+	var direction := Input.get_axis("move_left", "move_right")
+	
+	# Flip the direction	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	#	Play the  animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")	
+	else:
+		animated_sprite.play("jump")
+	
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
